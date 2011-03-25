f658b181e61f053bd7d6ae006c9b2c3c36eca3a4
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2011-03-26 02:05:35 +0530

    added more plugins

--- polkabot.rb
+++ polkabot.rb
@@ -1,12 +1,14 @@
 $:.unshift(Dir.pwd)
 require 'cinch'
 require 'youtube'
+require 'seen'
+require 'hello'
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.org"
     c.channels = ["#chennai-hackers"]
     c.nick = "polkabot"
-    c.plugins.plugins = [Youtube]
+    c.plugins.plugins = [Youtube,Seen,Hello]
   end
 end
 
