726ee8c9268dd3084329f7f373a97dcd5882a560
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2011-03-26 22:09:51 +0530

    cleaned up the plugins

--- bye.rb
+++ bye.rb
@@ -5,7 +5,7 @@ class Bye
   
   plugin 'bye'
   match /polkabot[,:] (bye|ciao|night)$/, use_prefix: false
-  help "polkabot, bye - Polkabot greets you!"
+  help "polkabot, (bye|ciao|night) - Polkabot greets you!"
 
   def execute(m)
     m.reply "Bye, #{m.user.nick}"
